755fd9277e17f0e44ff2003879048b627a0d7b93
Binayakdsiitm <131290909+Binayakdsiitm@users.noreply.github.com>
2023-04-19 23:40:44 +0530

    Update Largestnumber.py

--- Largestnumber.py
+++ Largestnumber.py
@@ -6,10 +6,10 @@ def main():
   num1=st.number_input("Enter first number:")
   num2=st.number_input("Enter second number:")
   num3=st.number_input("Enter third number:")
-  if st.button("Find the largest number:")
-    if num1>=num2 and num1>=num3
+  if st.button("Find the largest number:"):
+    if num1>=num2 and num1>=num3:
       largest_num = num1
-    elif num2>=num1 and num2>=num3
+    elif num2>=num1 and num2>=num3:
       largest_num = num2
     else:
       largest_num = num3
